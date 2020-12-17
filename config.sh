c987be5cb196e86efda2bc4a9a19e2d823be8c61
Jeff Whitaker <jswhit@fastmail.fm>
2020-12-16 20:46:02 -0700

    check c compiler

--- config.sh
+++ config.sh
@@ -13,7 +13,7 @@ source h5py-wheels/config.sh
 function build_libs {
     build_hdf5
     build_curl
-    build_netcdf
+    build_netcdf2
 }
 
 function pip_opts {
@@ -24,4 +24,18 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     cp ../netcdf4-python/test/* .
     python run_all.py
+}
+
+function build_netcdf {
+    if [ -e netcdf-stamp ]; then return; fi
+    build_hdf5
+    build_curl
+    fetch_unpack https://github.com/Unidata/netcdf-c/archive/v${NETCDF_VERSION}.tar.gz
+    echo "Using C compiler: $(which gcc)"
+    echo "gcc version: `gcc -version`"
+    (cd netcdf-c-${NETCDF_VERSION} \
+        && ./configure --prefix=$BUILD_PREFIX --enable-dap \
+        && make -j4 \
+        && make install)
+    touch netcdf-stamp
 }
