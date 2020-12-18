18b07df269a7daf2561a97288c1910329cea7c33
Jeff Whitaker <jswhit@fastmail.fm>
2020-12-18 08:21:43 -0700

    build old openssl

--- config.sh
+++ config.sh
@@ -7,13 +7,15 @@ export NO_NET=1
 export MACOSX_DEPLOYMENT_TARGET="10.9"
 export NETCDF_VERSION="4.7.4"
 export HDF5_VERSION="1.12.0"
-export PERL_VERSION="5.16.0"
+# old openssl, since building new version requires perl 5.10.0
+export OPENSSL_ROOT=openssl-1.0.2u
+export OPENSSL_HASH=ecd0c6ffb493dd06707d38b14bb4d8c2288bb7033735606569d8f90f89669d16
 
 source h5py-wheels/config.sh
 
 function build_libs {
     build_hdf5
-    build_curl2
+    build_curl
     if [ -z "$IS_OSX" ] && [ $MB_ML_VER -eq 1 ]; then
        export CFLAGS="-std=gnu99 -Wl,-strip-all"
     fi
@@ -28,31 +30,4 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     cp ../netcdf4-python/test/* .
     python run_all.py
-}
-
-function build_curl2 {
-    if [ -e curl-stamp ]; then return; fi
-    local flags="--prefix=$BUILD_PREFIX"
-    if [ -n "$IS_OSX" ]; then
-        flags="$flags --with-darwinssl"
-    else  # manylinux
-        flags="$flags --with-ssl"
-	# Install new Perl because OpenSSL configure scripts require > 5.10.0.
-	echo "Old Perl version `perl -v`"
-	curl -L https://install.perlbrew.pl | bash
-	export PERLBREW_ROOT=/root/perl5/perlbrew
-	source ${PERLBREW_ROOT}/etc/bashrc
-	perlbrew install perl-${PERL_VERSION}
-	perlbrew use perl-${PERL_VERSION}
-	echo "New Perl version `perl -v`"
-        build_openssl
-    fi
-    fetch_unpack https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz
-    (cd curl-${CURL_VERSION} \
-        && if [ -z "$IS_OSX" ]; then \
-        LIBS=-ldl ./configure $flags; else \
-        ./configure $flags; fi\
-        && make -j4 \
-        && make install)
-    touch curl-stamp
 }
