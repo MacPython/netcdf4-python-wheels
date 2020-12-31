96bea6307c22fab90caf967cfc2a48a33f68fe0f
jswhit <whitaker.jeffrey@gmail.com>
2020-12-31 10:20:16 -0700

    didn't work, re-enable built-in curl on OSX

--- config.sh
+++ config.sh
@@ -15,12 +15,13 @@ source h5py-wheels/config.sh
 
 function build_libs {
     build_hdf5
-    build_curl
-    #if [ -z "$IS_OSX" ]; then
-    #  build_curl
-    #else
-    #  touch curl-stamp
-    #fi
+    #build_curl
+    # use built-in curl on OSX
+    if [ -z "$IS_OSX" ]; then
+      build_curl
+    else
+      touch curl-stamp
+    fi
     if [ -z "$IS_OSX" ] && [ $MB_ML_VER -eq 1 ]; then
        export CFLAGS="-std=gnu99 -Wl,-strip-all"
     fi
