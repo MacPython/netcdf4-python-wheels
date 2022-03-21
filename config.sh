837b9a7ac491056e4afc365af2af6951c1927698
jswhit <whitaker.jeffrey@gmail.com>
2022-03-20 19:24:43 -0600

    build on only 3.10 on macosx-latest

--- config.sh
+++ config.sh
@@ -86,6 +86,12 @@ function build_libs {
     build_netcdf
 }
 
+#function pre_build {
+#    # Any stuff that you need to do before you start building the wheels
+#    # Runs in the root directory of this repository.
+#    build_zlib
+#}
+
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     pwd
