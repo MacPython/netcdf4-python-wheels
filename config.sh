34d4f92c63300bbe36e298e7f090bb8e112f3ea9
Jeff Whitaker <jswhit@fastmail.fm>
2022-06-18 15:40:21 -0600

    fix typo

--- config.sh
+++ config.sh
@@ -80,11 +80,11 @@ function build_netcdf {
     if [ -e netcdf-stamp ]; then return; fi
     fetch_unpack https://downloads.unidata.ucar.edu/netcdf-c/${NETCDF_VERSION}/netcdf-c-${NETCDF_VERSION}.tar.gz
     (cd netcdf-c-${NETCDF_VERSION} \
-        mkdir build \
-	cd build \
-	cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD_PREFIX} -DENABLE_NETCDF_4=ON -DENABLE_DAP=ON -DBUILD_SHARED_LIBS=ON  \
-	make -j4 \
-        make install)
+        && mkdir build \
+	&& cd build \
+	&& cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD_PREFIX} -DENABLE_NETCDF_4=ON -DENABLE_DAP=ON -DBUILD_SHARED_LIBS=ON  \
+	&& make -j4 \
+	&& make install)
     #(cd netcdf-c-${NETCDF_VERSION} \
     #    && ./configure --prefix=$BUILD_PREFIX --enable-netcdf-4 --enable-shared --enable-dap \
     #    && make -j4 \
