89a77e9b2f4d7a11c0fd0f3345ca6cb37d7d8d6b
jswhit <whitaker.jeffrey@gmail.com>
2022-06-22 13:16:34 -0600

    use cmake for everything, don't include plugins

--- config.sh
+++ config.sh
@@ -4,6 +4,7 @@
 export NO_NET=1
 # ncdump/ncgen not installed in wheel, so tst_cdl.py fails
 export NO_CDL=1
+export NO_PLUGINS=1
 
 # Compile libs for macOS 10.9 or later
 export MACOSX_DEPLOYMENT_TARGET="10.9"
@@ -20,11 +21,11 @@ export BZIP2_VERSION="1.0.8"
 export BLOSC_VERSION="1.21.1"
 
 # custom version that sets NETCDF_PLUGIN_DIR env var
-function build_wheel {
-    # Set default building method to pip
-    export NETCDF_PLUGIN_DIR=${BUILD_PREFIX}/lib/netcdf-plugins
-    wrap_wheel_builder build_pip_wheel $@
-}
+#function build_wheel {
+#    # Set default building method to pip
+#    export NETCDF_PLUGIN_DIR=${BUILD_PREFIX}/lib/netcdf-plugins
+#    wrap_wheel_builder build_pip_wheel $@
+#}
 
 # add --verbose to pip
 function pip_opts {
@@ -116,34 +117,41 @@ function build_netcdf {
     #    && make install \
     #    && ls -l $HDF5_PLUGIN_PATH )
     # autotools build
-    if [ -n "$IS_MACOS" ]; then
-       #if [[ ! -z "IS_OSX"  && "$PLAT" = "arm64" ]] && [[ "$CROSS_COMPILING" = "1" ]]; then
-       if [[ "$PLAT" = "arm64" ]] && [[ "$CROSS_COMPILING" = "1" ]]; then
-          (cd netcdf-c-${NETCDF_VERSION} \
-              && ./configure --prefix=$BUILD_PREFIX --enable-netcdf-4 --enable-shared --enable-dap \
-              && make -j4 \
-              && make install )
-       else
-          (cd netcdf-c-${NETCDF_VERSION} \
-              && export HDF5_PLUGIN_PATH=$BUILD_PREFIX/lib/netcdf-plugins \
-              && ./configure --prefix=$BUILD_PREFIX --enable-netcdf-4 --enable-shared --enable-dap --with-plugin-dir=$HDF5_PLUGIN_PATH \
-              && make -j4 \
-              && make install )
-       fi
-    else # use cmake for linux since autoconf fails for 4.9.0
-#          && cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD_PREFIX} -DENABLE_NETCDF_4=ON -DENABLE_DAP=ON -DBUILD_SHARED_LIBS=ON -DPLUGIN_INSTALL_DIR=YES \
-#          && export HDF5_PLUGIN_PATH=$BUILD_PREFIX/lib/netcdf-plugins \
-#          && mkdir -p $HDF5_PLUGIN_PATH \
+    #if [ -n "$IS_MACOS" ]; then
+    #   #if [[ ! -z "IS_OSX"  && "$PLAT" = "arm64" ]] && [[ "$CROSS_COMPILING" = "1" ]]; then
+    #   if [[ "$PLAT" = "arm64" ]] && [[ "$CROSS_COMPILING" = "1" ]]; then
+    #      (cd netcdf-c-${NETCDF_VERSION} \
+    #          && ./configure --prefix=$BUILD_PREFIX --enable-netcdf-4 --enable-shared --enable-dap \
+    #          && make -j4 \
+    #          && make install )
+    #   else
+    #      (cd netcdf-c-${NETCDF_VERSION} \
+    #          && export HDF5_PLUGIN_PATH=$BUILD_PREFIX/lib/netcdf-plugins \
+    #          && ./configure --prefix=$BUILD_PREFIX --enable-netcdf-4 --enable-shared --enable-dap --with-plugin-dir=$HDF5_PLUGIN_PATH \
+    #          && make -j4 \
+    #          && make install )
+    #   fi
+    #else # use cmake for linux since autoconf fails for 4.9.0
+       #(cd netcdf-c-${NETCDF_VERSION} \
+       #    && curl https://raw.githubusercontent.com/MacPython/netcdf4-python-wheels/master/CMakeLists.txt.patch -o CMakeLists.txt.patch \
+       #    && patch -p0 < CMakeLists.txt.patch \
+       #    && mkdir build \
+       #    && cd build \
+       #    && export HDF5_PLUGIN_PATH=$BUILD_PREFIX/lib/netcdf-plugins \
+       #    && mkdir -p $HDF5_PLUGIN_PATH \
+       #    && cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD_PREFIX} -DENABLE_NETCDF_4=ON -DENABLE_DAP=ON -DBUILD_SHARED_LIBS=ON -DPLUGIN_INSTALL_DIR=YES \
+       #    && make -j4 \
+       #    && make install \
+       #    && ls -l $HDF5_PLUGIN_PATH )
        (cd netcdf-c-${NETCDF_VERSION} \
            && curl https://raw.githubusercontent.com/MacPython/netcdf4-python-wheels/master/CMakeLists.txt.patch -o CMakeLists.txt.patch \
            && patch -p0 < CMakeLists.txt.patch \
            && mkdir build \
            && cd build \
-           && cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD_PREFIX} -DENABLE_NETCDF_4=ON -DENABLE_DAP=ON -DBUILD_SHARED_LIBS=ON -DENABLE_PLUGIN_INSTALL=NO=YES \
+           && cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD_PREFIX} -DENABLE_NETCDF_4=ON -DENABLE_DAP=ON -DBUILD_SHARED_LIBS=ON -DENABLE_PLUGIN_INSTALL=NO \
            && make -j4 \
-           && make install \
-           && ls -l $HDF5_PLUGIN_PATH )
-    fi
+           && make install )
+    #fi
     touch netcdf-stamp
 }
 
